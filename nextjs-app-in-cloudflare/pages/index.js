46f1a0eb45b55939cbfa136c83b0a4e34e9acc0b
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2023-04-20 17:10:22 -0500

    just messing around

--- nextjs-app-in-cloudflare/pages/index.js
+++ nextjs-app-in-cloudflare/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">The thing!</a>
+        Welcome to <a href="/">The thing!</a>
         </h1>
         <div className={styles.card}>
           <ul>
