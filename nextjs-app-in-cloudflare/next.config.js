23bb50cd7209afe726684ed6ee2edc83091a2cda
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2023-04-20 17:04:20 -0500

    build errors

--- nextjs-app-in-cloudflare/next.config.js
+++ nextjs-app-in-cloudflare/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    runtime: 'edge',
+    runtime: 'experimental-edge',
   },
   eslint:{
     ignoreDuringBuilds: true
